211a1db9f7203312328abb125286711eee69ee9f
mywonkim <66997713+mywonkim@users.noreply.github.com>
2020-12-25 14:55:34 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,119 @@
+import sys
+from PyQt5.QtWidgets import *
+import PyQt5.QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import QFont, QIcon
+from stockPrice import StockMarketPrice
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+
+# https://hello-bryan.tistory.com/213
+# https://pythonpyqt.com/pyqt-qlineedit/
+# https://pythonprogramminglanguage.com/pyqt-line-edit/
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        # form
+        self.setWindowTitle('Stock Price Live and Prediction')
+        self.center() # self.move(500,200)    # 창 위치
+        self.resize(700, 600)   # 창 크기
+
+        # input ticker symbol
+        self.tickerLabel = QLabel(self)
+        self.tickerLabel.setText('Enter ticker symbol of your company:')
+        self.tickerLabel.setFont(QFont("Arial", 20))
+        self.tickerLine = QLineEdit(self)
+
+        self.tickerLabel.move(20, 20)
+        self.tickerLine.move(20, 70)
+        self.tickerLine.resize(400, 32)
+
+        self.label = QLabel(self)
+        self.label.setText("1. Enter the ticker symbol of your company \nand click \"Enter\" button\n"
+                           "2. Make sure the message \"TICKER \nSYMBOL OF YOUR COMPANY HAS \nBEEN ENTERED!!\" shows up\n"
+                           "3. Click one of the three buttons: Live Price, \nPrice Prediction, or Price Trend\n"
+                           "4. Make sure you know this application has \nlimits and does not guarantee any accuracy\n"
+                           "5. Use this application at your own risk\n")
+        self.label.setFont(QFont("Arial", 20))
+        self.label.move(20, 160)
+
+        # btnEnter
+        btnEnter = QPushButton('Enter', self)
+        btnEnter.clicked.connect(self.stockPriceInfo)
+        btnEnter.resize(100, 32)
+        btnEnter.move(430, 70)
+
+        # btnLive
+        btnLive = QPushButton("Live Price", self)  # 버튼 텍스트
+        btnLive.move(20, 120)  # 버튼 위치
+        btnLive.clicked.connect(self.displayLivePrice)  # 클릭 시 실행할 function
+
+        # btnPredict
+        btnPredict = QPushButton("Price Prediction", self)  # 버튼 텍스트
+        btnPredict.move(120, 120)  # 버튼 위치
+        btnPredict.clicked.connect(self.predictPrice)  # 클릭 시 실행할 functionAAPL
+
+        #btnTrend
+        btnTrend = QPushButton("Price Trend", self)  # 버튼 텍스트
+        btnTrend.move(223, 120)  # 버튼 위치
+        btnTrend.clicked.connect(self.displayPriceTrend)  # 클릭 시 실행할 function
+
+    def tickerSymbolEntered(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("TICKER SYMBOL OF YOUR COMPANY HAS BEEN ENTERED!!")
+        msg.setWindowTitle("Information")
+        msg.exec_()
+
+    def predictPrice(self):
+        whichCol, done1 = QInputDialog.getText(
+            self, 'Open, High, Low, or Close', 'Enter what type of stock prices you want to predict: Open, High, Low, or Close: ')
+
+        howManyDaysAfterTodayToPredict, done2 = QInputDialog.getInt(
+            self, 'How many days after today?', 'You want to predict stock price in how many days?: ')
+
+        if done1 and done2:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setText(self.spi.pricePrediction(whichCol, howManyDaysAfterTodayToPredict))
+            msg.setWindowTitle("Information")
+            msg.exec_()
+
+    def displayPriceTrend(self):
+        self.spi.plotTrend()
+
+    def displayLivePrice(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText(self.spi.displayLivePrice())
+        msg.setWindowTitle("Information")
+        msg.exec_()
+
+    def printprint(self):
+        print(self.tickerLine.text())
+
+    # window at the center of my monitor
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    # initialize StockMarketPrice object
+    def stockPriceInfo(self):
+        self.spi = StockMarketPrice(self.tickerLine.text())
+        self.tickerSymbolEntered()
+
+def main():
+   app = QApplication(sys.argv)
+   ex = App()
+   ex.show()
+   sys.exit(app.exec_())
+
+if __name__ == '__main__':
+   main()
---
+++ stockPrice.py
@@ -0,0 +1,85 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import time
+import numpy as np
+import pandas_datareader as pdr
+from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+
+class StockMarketPrice:
+    def __init__(self, tickerSymbol):
+        self.tickerSymbolOfCompany = tickerSymbol
+        self.url = f"https://finance.yahoo.com/quote/{self.tickerSymbolOfCompany}/history?p={self.tickerSymbolOfCompany}"
+        self.getRequest = requests.get(self.url).text
+        # xml very fast and lenient
+        self.soup = BeautifulSoup(self.getRequest, 'lxml')
+        self.df = self.getDataForHD()
+        self.currency = self.getCurrency()
+
+    def getCurrency(self):
+        currency = self.soup.find('span', {'class' : 'Fz(xs)'}).find_all('span')[0].text
+        return currency
+
+    def getDataForLive(self):
+        price = self.soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('span')[0].text
+        priceDelta = self.soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('span')[1].text
+        return str(price) + " " + str(priceDelta)
+
+    def displayLivePrice(self):
+        now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        return f"Current price for {self.tickerSymbolOfCompany} at {now} is {self.getDataForLive()} ({self.currency})"
+        """
+        # update current price every minute
+        starttime = time.time()
+        while True:
+            print(f"current live price for {self.tickerSymbolOfCompany} is {self.getDataForLive()}")
+            time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+        """
+
+    def getDataForHD(self):
+        # historical data dataframe
+        # data from 10 years ago from now
+        historicalDF = \
+            pdr.get_data_yahoo(symbols=self.tickerSymbolOfCompany,
+                               start=datetime.today() - relativedelta(years=1), end=datetime.today())
+        # include Date index
+        historicalDF.reset_index(inplace=True, drop=False)
+        return historicalDF
+
+    def pricePrediction(self, whichCol, howManyDaysAfterTodayToPredict):
+        date_time = self.df['Date']
+        # starting date of historical date is 0
+        indepVar = np.arange(date_time.size)
+        # making dependent variable a 2d numpy array
+        _2DindepVar = np.reshape(indepVar, (-1, 1))
+
+        whichDateToPredict = _2DindepVar[len(_2DindepVar) - 1][0] + howManyDaysAfterTodayToPredict
+
+        # what to predict
+        depVar = self.df[whichCol].to_numpy()
+        _2DdepVar = np.reshape(depVar, (-1, 1))
+
+        # polynomial regression
+        poly_features = PolynomialFeatures(degree=10)
+        # contain original independent variable and its new features
+        indepVar_poly = poly_features.fit_transform(_2DindepVar)
+        model = linear_model.LinearRegression()
+        model.fit(indepVar_poly, _2DdepVar)  # Fit the model
+        to_predict = poly_features.fit_transform(np.array([[whichDateToPredict]]))
+        predicted = model.predict(to_predict)
+        pricePredicted = "{:.2f}".format(predicted[0][0])
+        # print predicted High
+        return f"Predicted {whichCol} price for {self.tickerSymbolOfCompany} in {howManyDaysAfterTodayToPredict} day(s) is {pricePredicted} ({self.currency})"
+
+
+    def plotTrend(self):
+        # https://queirozf.com/entries/pandas-dataframe-plot-examples-with-matplotlib-pyplot
+        ax = plt.gca()
+
+        self.df.plot(kind='line', x='Date', y='Close', ax=ax)
+        self.df.plot(kind='line', x='Date', y='High', color='red', ax=ax)
+        self.df.plot(kind='line', x='Date', y='Low', color='blue', ax=ax)
+        plt.show()
